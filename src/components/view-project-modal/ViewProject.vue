57d3d0053193bfaf7e000581c69cd7940536a6b1
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-04-29 23:17:02 +0100

    initial view project description modal

---
+++ src/components/view-project-modal/ViewProject.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main-container">
+    <img :src="imageLink" alt="" class="project-image" />
+    <p class="title">{{ projectName }}</p>
+    <p class="description">{{ description }}</p>
+
+    <template v-for="(skill, index) in skills" :key="index">
+      <img
+        :src="require(`../../assets/img/skills-bw/${skill}.svg`)"
+        alt=""
+        class="skill-image"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewProject",
+  props: {
+    projectName: {
+      type: String,
+      required: true,
+    },
+    imageLink: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    skills: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "./ViewProject.scss";
+</style>
