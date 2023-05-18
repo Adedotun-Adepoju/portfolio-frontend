13ae46b3ddc0cd9c2a46e8501683dcf869cb7ec7
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-05-18 22:52:24 +0100

    add backend url

--- src/api/portfolio.js
+++ src/api/portfolio.js
@@ -2,13 +2,15 @@ import axios from "axios";
 
 async function fetchExperiences() {
   const response = await axios.get(
-    `http://localhost:3000/portfolio/experiences`
+    `https://portfolio-backend-c3wk.onrender.com/portfolio/experiences`
   );
   return response;
 }
 
 async function fetchProjects() {
-  const response = await axios.get(`http://localhost:3000/portfolio/projects`);
+  const response = await axios.get(
+    `https://portfolio-backend-c3wk.onrender.com/portfolio/projects`
+  );
   return response;
 }
 
