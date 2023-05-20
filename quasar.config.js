a56b04f2fa60d098e107e7bfbec7ef492b3face0
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-05-20 11:22:49 +0100

    Remove overflow for mobile

--- quasar.config.js
+++ quasar.config.js
@@ -144,8 +144,8 @@ module.exports = configure(function (ctx) {
       },
 
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Adedotun Adepoju`,
+        short_name: `Adedotun Adepoju`,
         description: `A Quasar Project`,
         display: "standalone",
         orientation: "portrait",
